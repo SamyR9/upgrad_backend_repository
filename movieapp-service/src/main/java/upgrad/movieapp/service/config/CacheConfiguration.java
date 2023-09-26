3d6f3e1e80a0fb43655f0ab7893212f9007f7d69
Samiksha Rana <samikshasinghrana@gmail.com>
2023-09-26 20:16:51 +0530

    test commit

--- movieapp-service/src/main/java/upgrad/movieapp/service/config/CacheConfiguration.java
+++ movieapp-service/src/main/java/upgrad/movieapp/service/config/CacheConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableCaching
+@EnableCaching// xyz
 public class CacheConfiguration {
 
     @Bean
